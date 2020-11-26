ed1a50291cc4cde83d1148d887327374148bac66
qiu <1395133179@qq.com>
2020-11-26 15:34:34 +0800

    origin

--- README.md
+++ README.md
@@ -13,16 +13,43 @@ requirement:
 
 ```bash
 conda create -n unetpp python=3.7
+conda activate unetpp
 conda install pytorch==1.4.0 torchvision==0.5.0 cudatoolkit=10.1 -c pytorch
 conda install tqdm tensorboardX 
 ```
-download respons
-
+download this repository
+```bash
+git clone xxxxxx
+cd unetpp_pytorch 
+```
 use email notification
-
+```bash
+cp .email_setting.example ~/.email_setting
+gedit ~/.email_setting
+```
+prepare dataset
+```
+# not public now
+```
 
 usage
 ----------------------------------
-
- 
-```main.py```
+###train and test
+modify config file and run main.py to train and test.
+```bash
+python main.py
+```
+for test only, find _**'work_phase'**_ in config file and replace **_'train-val-test'_**
+ with **_'test'_**.
+###show
+after testing, run show.py to save result.
+```bash
+python show.py
+```
+result are saved in _**'workdir'**_/predict, from left to right they are: 
+fake rgb image, prediction, ground truth and concated image.
+###show_hyper
+advanced show.py with cv2 gui.
+```bash
+python show_hyper.py
+```
--- tool/send_email.py
+++ tool/send_email.py
@@ -6,20 +6,23 @@ def send_email(content, header):
     # print(header)
     # print(content)
     # return
-    with open(os.path.join(os.path.expanduser('~'),'.email_setting')) as f:
-        lines = f.readlines()
-        from_addr = lines[0].strip()
-        password = lines[1].strip()
-        smtp_server = lines[2].strip()
-        to_addr = lines[3].strip()
+    try:
+        with open(os.path.join(os.path.expanduser('~'),'.email_setting')) as f:
+            lines = f.readlines()
+            from_addr = lines[0].strip()
+            password = lines[1].strip()
+            smtp_server = lines[2].strip()
+            to_addr = lines[3].strip()
 
-    str_msg = content
-    msg = MIMEText(str_msg, 'plain', 'utf-8')
-    msg['From'] = Header(from_addr)
-    msg['To'] = Header(to_addr)
-    msg['Subject'] = Header(header)
-    server = smtplib.SMTP_SSL(host=smtp_server)
-    server.connect(smtp_server, 465)
-    server.login(from_addr, password)
-    server.sendmail(from_addr, to_addr, msg.as_string())
-    server.quit()
+        str_msg = content
+        msg = MIMEText(str_msg, 'plain', 'utf-8')
+        msg['From'] = Header(from_addr)
+        msg['To'] = Header(to_addr)
+        msg['Subject'] = Header(header)
+        server = smtplib.SMTP_SSL(host=smtp_server)
+        server.connect(smtp_server, 465)
+        server.login(from_addr, password)
+        server.sendmail(from_addr, to_addr, msg.as_string())
+        server.quit()
+    except:
+        print('send email failed!')
