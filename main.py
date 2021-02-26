8a0e7d0b0e88acb1a6928666b2a1e10047387a3e
qiu <1395133179@qq.com>
2021-02-26 16:39:03 +0800

    1127

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from model.unet import unet
-from model.unetpp import unetpp
-from model.u2net import u2net
+from model.model_factory import get_model
 from dataset.hyper_dataset import hyper_dataset
+from configs import config_factory
 from tool import send_email
 from tool import metric
-from configs import config_unet, config_unetpp, config_u2net
 
 import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
@@ -14,12 +13,7 @@ from tensorboardX import SummaryWriter
 from tqdm import tqdm
 import datetime
 
-# using_config = config_unet
-using_config = config_unetpp
-# using_config = config_u2net
-
-for config in using_config.all_configs:
-
+for config in config_factory.all_configs:
     config_str = '\n***config***\n'
     for k,v in config.items():
         config_str += '{}: {}\n'.format(k, v)
@@ -32,41 +26,31 @@ for config in using_config.all_configs:
     log.write(config_str)
 
     ### build model
-    if config['model'] == 'unet':
-        model = unet(config).cuda()
-    elif config['model'] == 'unetpp':
-        model = unetpp(config).cuda()
-    elif config['model'] == 'u2net':
-        model = u2net(config).cuda()
-    else:
-        print('model not implemented!')
-        send_email.send_email(config_str, config['model'] + ' model not implemented!')
-        continue
+    model = get_model(config)
+    model_str = '\n***model***\n{}'.format(model)
+    print(model_str)
+    log.write(model_str)
+    # config_str += model_str
 
     ## build optimizer
     if config['optimizer'] == 'Adam':
         optimizer = optim.Adam(model.parameters(), lr=config['learning_rate'], weight_decay=config['weight_decay'])
+        scheduler = optim.lr_scheduler.MultiStepLR(optimizer, config['scheduler'], gamma=config['scheduler_rate'])
+    elif config['optimizer'] == 'SGD':
+        optimizer = optim.SGD(model.parameters(), lr=config['learning_rate'], weight_decay=config['weight_decay'], momentum=0.9)
         scheduler = optim.lr_scheduler.MultiStepLR(optimizer, config['scheduler'], gamma=config['scheduler_rate'])
     else:
         print('optimizer not implemented!')
         send_email.send_email(config_str, config['optimizer'] + ' optimizer not implemented!')
         continue
 
-    ## build loss
-    if config['loss'] == 'BCE':
-        loss_func = torch.nn.BCELoss()
-    else:
-        print('loss not implemented!')
-        send_email.send_email(config_str, config['loss'] + ' loss not implemented!')
-        continue
-
     ## build dataset
     if config['dataset'] == 'hyper':
-        train_dataset = hyper_dataset(config['train_dir'], config['norm_kwargs'], config['channel_transform'])
+        train_dataset = hyper_dataset(config['npy_dir'], config['label_dir'], config['train_split'], config['norm_kwargs'], config['channel_transform'], config['aug_config'])
         train_loader = DataLoader(train_dataset, batch_size=config['batch_size'], shuffle=True)
-        val_dataset = hyper_dataset(config['val_dir'], config['norm_kwargs'], config['channel_transform'])
+        val_dataset = hyper_dataset(config['npy_dir'], config['label_dir'], config['val_split'], config['norm_kwargs'], config['channel_transform'], config['aug_config'])
         val_loader = DataLoader(val_dataset, batch_size=1, shuffle=True)
-        test_dataset = hyper_dataset(config['test_dir'], config['norm_kwargs'], config['channel_transform'])
+        test_dataset = hyper_dataset(config['npy_dir'], config['label_dir'], config['test_split'], config['norm_kwargs'], config['channel_transform'], config['aug_config'])
         test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
     else:
         print('dataset not implemented!')
@@ -84,17 +68,16 @@ for config in using_config.all_configs:
             for step, (batch_x, batch_y) in enumerate(tqdm(train_loader)):
                 batch_x = batch_x.cuda()
                 batch_y = batch_y.cuda()
-                prediction = model(batch_x)
-                loss = torch.tensor(0, dtype=torch.float32).cuda()
-                for pred in prediction:
-                    loss += loss_func(pred, batch_y)
-                # print('epoch:{}, step:{}, train loss:{}'.format(epoch, step, loss))
-                log.write('+++time:{}, epoch:{}, step:{}, train loss:{}\n'.format(datetime.datetime.now(), epoch, step, loss))
-                writer.add_scalar('trainloss', loss.item(), global_step=step + epoch * train_dataset.len)
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-            scheduler.step(epoch)
+                logits = model(batch_x)
+                loss = model.get_loss(logits, batch_y)
+                with torch.no_grad():
+                    # print('epoch:{}, step:{}, train loss:{}'.format(epoch, step, loss))
+                    log.write('+++time:{}, epoch:{}, step:{}, train loss:{}\n'.format(datetime.datetime.now(), epoch, step, loss))
+                    writer.add_scalar('trainloss', loss.item(), global_step=step + epoch * train_dataset.len)
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+            scheduler.step()
             if epoch % config['save_interval'] == config['save_interval'] - 1:
                 torch.save(model.state_dict(), os.path.join(config['workdir'] ,"epoch_{}.pth".format(epoch)))
             torch.cuda.empty_cache()
@@ -107,18 +90,16 @@ for config in using_config.all_configs:
                 for step, (batch_x, batch_y) in enumerate(tqdm(val_loader)):
                     batch_x = batch_x.cuda()
                     batch_y = batch_y.cuda()
-                    prediction = model(batch_x)
-                    loss = torch.tensor(0, dtype=torch.float32).cuda()
-                    for pred in prediction:
-                        loss += loss_func(pred, batch_y)
+                    logits = model(batch_x)
+                    loss = model.get_loss(logits, batch_y)
                     # print('epoch:{}, step:{}, train loss:{}'.format(epoch, step, loss))
                     log.write('---time:{}, epoch:{}, step:{}, test loss:{}\n'.format(datetime.datetime.now(), epoch, step, loss))
                     writer.add_scalar('testloss', loss.item(), global_step=step + epoch * test_dataset.len)
                     optimizer.zero_grad()
                     ### metric
-                    prediction = prediction[-1].detach().cpu().numpy()
-                    batch_y = batch_y.detach().cpu().numpy()
-                    metrics_.append(metric.get_metrics(prediction, batch_y))
+                    prediction = model.get_predict(logits)
+                    gt = model.get_gt(batch_y)
+                    metrics_.append(metric.get_metrics(prediction, logits, gt))
                 result_str = '\n***epoch_{}_result***\n'.format(epoch)
                 for k,v in metric.show_metrics(metrics_).items():
                     result_str += '{}: {}\n'.format(k, v)
@@ -126,6 +107,7 @@ for config in using_config.all_configs:
                 log.write(result_str)
                 if metric.show_metrics(metrics_)['kappa'] > best_val_score:
                     torch.save(model.state_dict(), os.path.join(config['workdir'], "best_val_score.pth".format(epoch)))
+                    best_val_score = metric.show_metrics(metrics_)['kappa']
                 torch.cuda.empty_cache()
 
 
@@ -145,11 +127,11 @@ for config in using_config.all_configs:
                 for step, (batch_x, batch_y) in enumerate(tqdm(test_loader)):
                     batch_x = batch_x.cuda()
                     # batch_y = batch_y.cuda()
-                    prediction = model(batch_x)
+                    logits = model(batch_x)
                     ### metric
-                    prediction = prediction[-1].detach().cpu().numpy()
-                    batch_y = batch_y.detach().cpu().numpy()
-                    metrics_.append(metric.get_metrics(prediction, batch_y))
+                    prediction = model.get_predict(logits)
+                    gt = model.get_gt(batch_y)
+                    metrics_.append(metric.get_metrics(prediction, logits, gt))
                 result_str = '\n***test_{}_result***\n'.format(test_epoch)
                 for k, v in metric.show_metrics(metrics_).items():
                     result_str += '{}: {}\n'.format(k, v)
